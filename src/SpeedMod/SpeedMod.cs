54e59e571f236e1c411fa3e72f9b333b2bfcc3c4
VR-CATMAN <209883974+VR-CATMAN@users.noreply.github.com>
2025-05-01 17:56:49 +0900

    add speedmod.cs
    Added high speed mod code to double the speed of i am cat npcs

---
+++ src/SpeedMod/SpeedMod.cs
@@ -0,0 +1,54 @@
+﻿// SimpleTimeScaleMod.cs
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(SimpleTimeScaleMod), "SimpleTimeScaleMod", "1.0.0", "YourName")]
+[assembly: MelonGame("New Folder Games", "I Am Cat")]
+
+public class SimpleTimeScaleMod : MelonMod
+{
+    public override void OnApplicationStart()
+    {
+        MelonLogger.Msg("SimpleTimeScaleMod loaded! Time.timeScale = 2");
+    }
+
+    public override void OnUpdate()
+    {
+        // ここを好きな倍率に変えれば OK
+        Time.timeScale = 2f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;  // 物理挙動の同期用
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
